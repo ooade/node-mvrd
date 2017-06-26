8abb79c1d58ea43f049dd37fd4f5ba534fccc8ba
Ademola Adegbuyi <ooade96@gmail.com>
2017-06-26 21:18:36 +0100

    init

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const fetch = require('isomorphic-unfetch')
+const cheerio = require('cheerio')
+
+const app = express()
+
+const URL_TO_CRAWL = 'http://lsmvaapvs.org'
+
+const PORT = process.env.PORT || 3000
+
+app.get('/api/:id', (req, res) => {
+	fetch(`${URL_TO_CRAWL}/search.php?vpn=${req.params.id}`)
+		.then(res => res.text())
+		.then(html => {
+			let $ = cheerio.load(html)
+
+			let plateNumber,
+				color,
+				model,
+				chasisNumber,
+				vehicleStatus,
+				issueDate,
+				expiryDate
+
+			let count = 0
+
+			$('.col-sm-8').filter(function() {
+				let data = $(this).text()
+
+				switch (count) {
+					case 0:
+						plateNumber = data
+					case 1:
+						color = data
+					case 2:
+						model = data
+					case 3:
+						chasisNumber = data
+					case 4:
+						vehicleStatus = data
+					case 5:
+						issueDate = data
+					case 6:
+						expiryDate = data
+				}
+
+				count++
+			})
+
+			res.send({
+				color,
+				model,
+				plate_number: plateNumber,
+				chasis_number: chasisNumber,
+				vehicle_status: vehicleStatus,
+				issue_date: issueDate,
+				expiry_date: expiryDate
+			})
+		})
+})
+
+app.listen(PORT, () => {
+	console.log('> App running on PORT 3000')
+})
